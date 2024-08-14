d5a3a2e3aab04595f94dea2f0b707bbe21d0ddde
Sulwe <cypriansulwe.cs@gmail.com>
2024-08-14 12:17:51 +0300

    add popular and offers sections

--- ebike-store-frontend/src/App.js
+++ ebike-store-frontend/src/App.js
@@ -1,10 +1,30 @@
 import './App.css';
 import Navbar from './Components/Navbar/Navbar';
+import Shop from './Pages/Shop';
+import Products from './Pages/Products';
+import ShopCategory from './Pages/Shop';
+import Cart from './Pages/Cart';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import LoginSignup from './Pages/LoginSignup';
 
 function App() {
   return (
     <div>
+      <BrowserRouter>
       <Navbar/>
+      <Routes>
+        <Route path = '/' element = {<Shop/>}/>
+        <Route path = '/mens' element = {<ShopCategory category = 'mens'/>}/>
+        <Route path = '/womens' element = {<ShopCategory category = 'womens'/>}/>
+        <Route path = '/kids' element = {<ShopCategory category = 'kids'/>}/>
+        <Route path = '/products' element = {<Products/>}>
+          <Route path = ':productId' element = {<Products/>}/>
+        </Route>
+        <Route path = '/cart' element = {<Cart/>}/>
+        <Route path = '/login' element = {<LoginSignup/>}/>
+      </Routes>
+      </BrowserRouter>
+      
       
     </div>
   );
