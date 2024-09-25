e5c57ed3956b4d8425779563b4d7d23df4384f2c
Sulwe <cypriansulwe.cs@gmail.com>
2024-09-25 20:29:20 +0300

    add shop category

--- ebike-store-frontend/src/App.js
+++ ebike-store-frontend/src/App.js
@@ -2,11 +2,14 @@ import './App.css';
 import Navbar from './Components/Navbar/Navbar';
 import Shop from './Pages/Shop';
 import Products from './Pages/Products';
-import ShopCategory from './Pages/Shop';
+import ShopCategory from './Pages/ShopCategory';
 import Cart from './Pages/Cart';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import LoginSignup from './Pages/LoginSignup';
 import Footer from './Components/Footer/Footer';
+import mens_banner from './Components/Assets/banner_mens.png'
+import womens_banner from './Components/Assets/banner_women.png'
+import kids_banner from './Components/Assets/banner_kids.png'
 
 function App() {
   return (
@@ -15,9 +18,9 @@ function App() {
       <Navbar/>
       <Routes>
         <Route path = '/' element = {<Shop/>}/>
-        <Route path = '/mens' element = {<ShopCategory category = 'mens'/>}/>
-        <Route path = '/womens' element = {<ShopCategory category = 'womens'/>}/>
-        <Route path = '/kids' element = {<ShopCategory category = 'kids'/>}/>
+        <Route path = '/mens' element = {<ShopCategory banner={mens_banner} category = 'men'/>}/>
+        <Route path = '/womens' element = {<ShopCategory banner={womens_banner} category = 'women'/>}/>
+        <Route path = '/kids' element = {<ShopCategory banner={kids_banner} category = 'kid'/>}/>
         <Route path = '/products' element = {<Products/>}>
           <Route path = ':productId' element = {<Products/>}/>
         </Route>
